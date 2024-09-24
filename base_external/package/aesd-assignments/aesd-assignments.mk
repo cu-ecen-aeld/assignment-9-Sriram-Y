1abd4f9a664fb770b5c6d0eeabd1be7c81ae8bdf
Sriram-Y <sriramy2001@gmail.com>
2024-09-23 20:46:14 -0600

    aesd-assignments.mk

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = e3267e2119abef4389b7842a95d0d83e98e489c9
+AESD_ASSIGNMENTS_VERSION = 59ecf1bf14a93a5283902e0168d70812b437aece
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -22,10 +22,14 @@ endef
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -m 0777 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0777 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0777 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/
+	sed -i -e 's/..\/conf\/assignment.txt/\/etc\/finder-app\/conf\/assignment.txt/g' $(TARGET_DIR)/usr/bin/finder-test.sh
+	sed -i -e 's/conf\/username.txt/\/etc\/finder-app\/conf\/username.txt/g' $(TARGET_DIR)/usr/bin/finder-test.sh
+	sed -i -e 's/.\/writer/writer/g' $(TARGET_DIR)/usr/bin/finder-test.sh
+	sed -i -e 's/.\/finder.sh/finder.sh/g' $(TARGET_DIR)/usr/bin/finder-test.sh
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/writer $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/finder $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/finder-test $(TARGET_DIR)/bin
 endef
 
 $(eval $(generic-package))
