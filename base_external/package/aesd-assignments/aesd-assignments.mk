2e6f76e85344508c08231506f29f5a035e030d80
Sriram-Y <sriramy2001@gmail.com>
2024-10-09 04:32:59 +0000

    buildroot fixes, maybe

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 59ecf1bf14a93a5283902e0168d70812b437aece
+AESD_ASSIGNMENTS_VERSION = 0138e56afccca5ea99d4087ef56f426e6fcca5e1
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -25,11 +25,6 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0777 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0777 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0777 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/
-	sed -i -e 's/..\/conf\/assignment.txt/\/etc\/finder-app\/conf\/assignment.txt/g' $(TARGET_DIR)/usr/bin/finder-test.sh
-	sed -i -e 's/conf\/username.txt/\/etc\/finder-app\/conf\/username.txt/g' $(TARGET_DIR)/usr/bin/finder-test.sh
-	sed -i -e 's/.\/writer/writer/g' $(TARGET_DIR)/usr/bin/finder-test.sh
-	sed -i -e 's/.\/finder.sh/finder.sh/g' $(TARGET_DIR)/usr/bin/finder-test.sh
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
 endef
 
 $(eval $(generic-package))
--- clean.sh
+++ clean.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # Script to clean from the buildroot directory
 # Store the current directory
-ROOT_DIR=$(pwd)
 
-# Go to buildroot
-cd ${ROOT_DIR}/buildroot
 
-# Run make distclean
-make distclean
+if [ -d "buildroot"]; then
+    make -C buildroot distclean
+else
+    echo "No buildroot directory"
+    exit 1
+fi
+
+exit 0
