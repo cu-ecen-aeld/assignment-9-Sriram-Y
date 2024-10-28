1dda20baec32dbd3a5032a2743790b30b2e01c51
Sriram-Y <sriramy2001@gmail.com>
2024-10-28 03:41:59 +0000

    trying to fix loading and unloading aesdchar issues

---
+++ base_external/package/aesd-char-driver/aesd-char-driver.mk
@@ -0,0 +1,25 @@
+
+##############################################################
+#
+# AESDCHARDRIVER
+#
+##############################################################
+
+AESDCHARDRIVER_VERSION = '8ee0c3cd89758b5d47dc644f3cc45eab91e75786'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESDCHARDRIVER_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-Sriram-Y.git'
+AESDCHARDRIVER_SITE_METHOD = git
+AESDCHARDRIVER_GIT_SUBMODULES = YES
+
+AESDCHARDRIVER_MODULE_SUBDIRS = aesd-char-driver
+
+define AESDCHARDRIVER_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdchar
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
