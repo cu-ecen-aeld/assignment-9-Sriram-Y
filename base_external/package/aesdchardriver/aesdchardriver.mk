2015cc0d6f8cd9e06f6b32f60cbe0467afa76d49
Sriram-Y <sriramy2001@gmail.com>
2024-11-03 05:23:28 +0000

    added aesdchardriver package

---
+++ base_external/package/aesdchardriver/aesdchardriver.mk
@@ -0,0 +1,24 @@
+
+##############################################################
+#
+# AESDCHARDRIVER
+#
+##############################################################
+
+AESDCHARDRIVER_VERSION = '7588e9d63cf9844f66a6534b144485c92223c638'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESDCHARDRIVER_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-Sriram-Y.git'
+AESDCHARDRIVER_SITE_METHOD = git
+AESDCHARDRIVER_GIT_SUBMODULES = YES
+
+AESDCHARDRIVER_MODULE_SUBDIRS = aesd-char-driver
+
+define AESDCHARDRIVER_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin/
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
