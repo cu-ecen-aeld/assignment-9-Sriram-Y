71708b68d15cfb931b061f704149c1c56d5e69d9
Sriram-Y <sriramy2001@gmail.com>
2024-11-03 00:35:08 +0000

    updated commit hashes

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'f29c7aa080cc8e51b8e19ca0158d35f03880b40e'
+AESD_ASSIGNMENTS_VERSION = '7588e9d63cf9844f66a6534b144485c92223c638'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
--- base_external/package/aesdchardriver/aesdchardriver.mk
+++ base_external/package/aesdchardriver/aesdchardriver.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESDCHARDRIVER_VERSION = 'f29c7aa080cc8e51b8e19ca0158d35f03880b40e'
+AESDCHARDRIVER_VERSION = '7588e9d63cf9844f66a6534b144485c92223c638'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -18,7 +18,6 @@ AESDCHARDRIVER_MODULE_SUBDIRS = aesd-char-driver
 define AESDCHARDRIVER_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdchardriver
 endef
 
 $(eval $(kernel-module))
